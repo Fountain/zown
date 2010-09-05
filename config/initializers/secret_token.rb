0e2822465c0d5d498d99583b7f9477cdf3be097e
Brian Fountain <brian@brianfountain.com>
2010-09-05 15:14:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zown::Application.config.secret_token = 'b02cc34678932c94589ddfcfc1c182bbea83543f0fcd5ac6ab9100f62a7f7b8e5ae12b2f690afd23c66fdf78fd6f167002e156b8e99370c9ba8f922d9dcc8389'
