b7edbd223340c351d9c8a5ccdcdac6fe2691ccbf
Brian Fountain <brian@brianfountain.com>
2010-11-14 21:01:28 -0500

    added static home page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Zown::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   match 'capture' => 'captures#new'
+  match '/' => 'home#index'
     
   # Keep in mind you can assign values other than :controller and :action
 
@@ -61,7 +62,7 @@ Zown::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "users#index"
+  root :to => "home"
 
   # See how all your routes lay out with "rake routes"
 
